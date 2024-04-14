11837cdd4dca3fecae6e49870a5796013c487b7e
Oumaimatray <oumaymatray19@gmail.com>
2024-04-14 11:45:08 +0100

    add Recording and _pickMedia

--- front_end/mobile_app/lib/chat/Conversation.dart
+++ front_end/mobile_app/lib/chat/Conversation.dart
@@ -4,7 +4,15 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:mobile_app/chat/call.dart';
 import 'package:mobile_app/chat/vedio.dart';
 import 'package:intl/intl.dart'; // Ensure you have added the intl package for date formatting
-//import 'package:emoji_picker_flutter/emoji_picker_flutter.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:flutter_sound/flutter_sound.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'dart:io';
+import 'package:audioplayers/audioplayers.dart';
+import 'package:audioplayers/audioplayers.dart';
+import 'package:file_picker/file_picker.dart';
+
+enum MessageType { text, audio, file }
 
 // Assuming you have a Message class like this:
 class Message {
@@ -12,12 +20,17 @@ class Message {
   String text;
   DateTime timestamp;
   String senderAvatar; // Add this line
-
+  String? audioPath;
+  String? filePath;
+  MessageType type;
   Message({
     required this.senderId,
     required this.text,
     required this.timestamp,
-    required this.senderAvatar, // Add this line
+    required this.senderAvatar,
+    this.audioPath, // Add this line
+    this.type = MessageType.text,
+    this.filePath,
   });
 }
 
@@ -44,15 +57,98 @@ class _ConversationPageState extends State<ConversationPage> {
   final FocusNode _textFieldFocusNode = FocusNode();
   String currentUserId = 'yourCurrentUserId';
   String senderAvatar = 'assets/Ellipse 11.png';
+  String? _selectedFilePath;
+  bool _isFileSelected = false;
 
   late List<Message> messages;
   final TextEditingController _messageController = TextEditingController();
   final ScrollController _scrollController = ScrollController();
+  final FlutterSoundRecorder _audioRecorder = FlutterSoundRecorder();
 
   @override
   void initState() {
     super.initState();
     messages = [];
+    initRecorder();
+  }
+// Import required for file handling
+
+  void sendMessageWithAudio(String path) {
+    File audioFile = File(path);
+
+    // Create a new message object for the audio
+    Message newMessage = Message(
+      senderId: currentUserId,
+      text: "",
+      timestamp: DateTime.now(),
+      senderAvatar: senderAvatar,
+      type: MessageType.audio,
+      audioPath: path,
+    );
+    setState(() {
+      messages.add(newMessage);
+    });
+
+    // Optionally, upload the audio file to a server
+    uploadAudioFile(audioFile);
+  }
+
+  Future<void> initRecorder() async {
+    var hasPermission = await checkPermissions();
+    if (!hasPermission) {
+      print("Microphone permission not granted");
+      return;
+    }
+
+    try {
+      await _audioRecorder.openRecorder();
+    } catch (e) {
+      print("Failed to open the recorder: $e");
+      return;
+    }
+  }
+
+  Future<bool> checkPermissions() async {
+    var status = await Permission.microphone.request();
+    return status == PermissionStatus.granted;
+  }
+
+// Example placeholder function for uploading an audio file
+  Future<void> uploadAudioFile(File audioFile) async {
+    try {
+      // Implementation of file upload
+      // This can be done using various methods depending on how your backend is set up
+      // For example, using HTTP multipart requests, Firebase Storage, etc.
+      print("Audio file uploaded: ${audioFile.path}");
+    } catch (e) {
+      print("Failed to upload audio file: $e");
+    }
+  }
+
+  Future<void> startRecording() async {
+    try {
+      await _audioRecorder.startRecorder(toFile: 'audio_message.aac');
+    } catch (e) {
+      print("Failed to start recording: $e");
+      // Optionally, try to re-initialize the recorder if it fails to start
+      await initRecorder();
+      try {
+        await _audioRecorder.startRecorder(toFile: 'audio_message.aac');
+      } catch (e) {
+        print("Second attempt to start recording failed: $e");
+        // Handle further errors or notify the user as necessary
+      }
+    }
+  }
+
+  Future<void> stopRecordingAndSend() async {
+    final String? path = await _audioRecorder.stopRecorder();
+    if (path != null) {
+      sendMessageWithAudio(path); // Only call this if path is not null
+    } else {
+      // Optionally handle the situation where no recording was captured
+      print("Recording failed or was cancelled.");
+    }
   }
 
   void onCallButtonPressed() {
@@ -77,6 +173,30 @@ class _ConversationPageState extends State<ConversationPage> {
         ), // Your VideoPage class
       ),
     );
+  }
+
+  Future<void> _pickMedia() async {
+    try {
+      FilePickerResult? result = await FilePicker.platform.pickFiles(
+        type: FileType.image, // To pick only images, change as needed
+      );
+
+      if (result != null) {
+        setState(() {
+          _selectedFilePath = result.files.single.path;
+          _isFileSelected = true;
+          _messageController.text =
+              _selectedFilePath!.split('/').last; // Display file name in input
+        });
+      } else {
+        // User canceled the picker
+        setState(() {
+          _isFileSelected = false;
+        });
+      }
+    } catch (e) {
+      print('Failed to pick file: $e');
+    }
   }
 
   void _sendMessage() {
@@ -184,6 +304,25 @@ class _ConversationPageState extends State<ConversationPage> {
   // _showEmojiPicker = !_showEmojiPicker;
   // });
   // }
+// Assume you have a variable to hold the picked file path
+  String? pickedFilePath;
+
+// A widget to display the picked file or image
+  Widget displayPickedFile() {
+    if (pickedFilePath == null) {
+      return Text('No file selected');
+    } else {
+      return pickedFilePath!.endsWith('.jpg') ||
+              pickedFilePath!.endsWith('.jpeg') ||
+              pickedFilePath!.endsWith('.png') ||
+              pickedFilePath!.endsWith('.gif')
+          ? Image.file(File(pickedFilePath!))
+          : ListTile(
+              leading: Icon(Icons.file_present),
+              title: Text(pickedFilePath!.split('/').last),
+            );
+    }
+  }
 
   Widget _buildInputSection() {
     final BorderRadius borderRadius = BorderRadius.circular(30.0);
@@ -206,115 +345,144 @@ class _ConversationPageState extends State<ConversationPage> {
       child: Row(
         children: <Widget>[
           Expanded(
-            child: TextField(
-              controller: _messageController,
-              focusNode:
-                  _textFieldFocusNode, // Ensure you have a FocusNode for the text field
-              onTap: () {
-                // When the text field is tapped, ensure the emoji picker is not shown
-                setState(() {
-                  // _showEmojiPicker = false;
-                });
-              },
-              decoration: InputDecoration(
-                hintText: "Message",
-                contentPadding:
-                    EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
-                border: InputBorder.none,
-                suffixIcon: IconButton(
-                  icon: Image.asset('assets/emoji.png'),
-                  onPressed: () {
-                    // Check if the keyboard is already open
-                    // if (_textFieldFocusNode.hasFocus) {
-                    // If the keyboard is open, close it to show the emoji picker
-                    // SystemChannels.textInput.invokeMethod('TextInput.hide');
-                    // setState(() {
-                    // _showEmojiPicker = true;
-                    // });
-                    //} else {
-                    // If the keyboard is not open, request focus and open it
-                    // _textFieldFocusNode.requestFocus();
-                    // setState(() {
-                    // _showEmojiPicker = false;
-                    // });
-                    //   }
-                  },
-                ),
+              child: TextField(
+            controller: _messageController,
+            maxLines:
+                null, // Allows the input field to expand to accommodate the text
+            //softWrap: true, // Enables text wrapping
+            keyboardType: TextInputType.multiline, // Allows for multiline input
+            decoration: InputDecoration(
+              hintText: "Type a message",
+              contentPadding:
+                  EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+              border: InputBorder.none,
+              suffixIcon: IconButton(
+                iconSize: iconSize,
+                icon: Icon(Icons.attach_file),
+                onPressed: _pickMedia,
+              ),
+              labelStyle: TextStyle(
+                color: Colors.white, // Style the label text (e.g., color, size)
+                overflow: TextOverflow
+                    .ellipsis, // Add an ellipsis if the text overflows
               ),
             ),
-          ),
+          )),
 
-          IconButton(
-            iconSize: iconSize,
-            icon: Image.asset('assets/photo.png'),
-            onPressed: () {}, // Updated to call _pickImage
+          GestureDetector(
+            onLongPressStart: (_) => startRecording(),
+            onLongPressEnd: (_) => stopRecordingAndSend(),
+            child: IconButton(
+              iconSize: iconSize,
+              icon: Image.asset('assets/vocal.png'),
+              onPressed: null, // Disable the default press functionality
+            ),
           ),
-
-          IconButton(
-            iconSize: iconSize,
-            icon: Image.asset('assets/vocal.png'),
-            onPressed: () {
-              // Implement microphone functionality
-            },
-          ),
-          // Send button
           IconButton(
             iconSize: iconSize,
             icon: Icon(
-              Icons.send, // This uses the built-in send icon
-              color: Colors.white, // Change the color as needed
+              Icons.send, // Uses the built-in send icon
+              color: Colors.white, // Icon color
             ),
-            onPressed: _sendMessage, // Call your send message function
+            onPressed: _sendMessage, // Triggers sending a message
           ),
+
+          // Send button
         ],
       ),
     );
   }
 
-  Widget _buildMessage(Message message, bool isMe) {
-    // Couleurs de la bulle de message
+  Widget _buildMessage(
+    Message message,
+    bool isMe,
+  ) {
     Color messageBgColor = isMe ? Color(0xFFFFFFFF) : Color(0xFF937CC1);
-    Color messageTextColor = isMe ? Color(0xFF28243D) : Color(0xFFFFFFFF);
+    double maxWidth = MediaQuery.of(context).size.width *
+        0.6; // Decrease the max width to make the bubble smaller
 
-    // Temps formaté
-    String formattedTime = DateFormat('h:mm a').format(message.timestamp);
+    Widget messageContent = SizedBox.shrink();
+    switch (message.type) {
+      case MessageType.audio:
+        messageContent = Container(
+          height: 50.0,
+          width: MediaQuery.of(context).size.width * 0.6,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(20.0),
+          ),
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 8.0),
+            child: AudioPlayerWidget(audioPath: message.audioPath),
+          ),
+        );
+        break;
+      case MessageType.file:
+        messageContent = Container(
+          padding: EdgeInsets.all(8.0),
+          constraints: BoxConstraints(maxWidth: maxWidth),
+          decoration: BoxDecoration(
+            color: isMe ? Colors.lightBlueAccent : Colors.grey[200],
+            borderRadius: BorderRadius.circular(20.0),
+          ),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Icon(Icons.attach_file), // Change the icon for non-audio files
+              Expanded(
+                child: Text(
+                  message.filePath?.split('/').last ?? 'Unknown file',
+                  softWrap: true,
+                  style: TextStyle(
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  maxLines: 1,
+                  // Allow for text to span up to 2 lines
+                ),
+              ),
+              // You may want to conditionally show the duration for audio files
+            ],
+          ),
+        );
 
-    // Création du widget d'avatar
+      case MessageType.text:
+      default:
+        messageContent = Text(
+          message.text,
+          style: TextStyle(
+            color: isMe ? Color(0xFF28243D) : Color(0xFFFFFFFF),
+            fontSize: 16,
+          ),
+        );
+        break;
+    }
+
     Widget avatarWidget = Padding(
       padding: EdgeInsets.symmetric(horizontal: 8.0),
       child: CircleAvatar(
-        backgroundImage:
-            AssetImage(message.senderAvatar), // Chemin vers l'image d'avatar
+        backgroundImage: AssetImage(message.senderAvatar),
       ),
     );
 
-    // Widget de la bulle de message
     Widget messageBubble = Container(
-      margin: EdgeInsets.all(8.0),
-      padding: EdgeInsets.symmetric(horizontal: 15.0, vertical: 10.0),
+      margin: EdgeInsets.symmetric(
+          vertical: 4.0, horizontal: 8.0), // Adjust margins as needed
+      padding: EdgeInsets.symmetric(
+          horizontal: 12.0, vertical: 8.0), // Adjust padding as needed
       decoration: BoxDecoration(
         color: messageBgColor,
         borderRadius: BorderRadius.circular(20.0),
       ),
-      child: Text(
-        message.text,
-        style: TextStyle(
-          color: messageTextColor,
-          fontSize: 16.0,
-        ),
-      ),
+      child: messageContent,
     );
 
-    // Aligner l'avatar et la bulle de message dans un Row selon isMe
     return Row(
       mainAxisAlignment: isMe ? MainAxisAlignment.end : MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.end,
       children: <Widget>[
-        if (!isMe)
-          avatarWidget, // Si le message est reçu, placer l'avatar à gauche
+        if (!isMe) avatarWidget,
         messageBubble,
-        if (isMe)
-          avatarWidget, // Si le message est envoyé, placer l'avatar à droite
-        // Inclure un espace entre le message et l'avatar pour éviter le chevauchement
+        if (isMe) avatarWidget,
       ],
     );
   }
@@ -488,6 +656,142 @@ class _ConversationPageState extends State<ConversationPage> {
   @override
   void dispose() {
     _overlayEntry?.remove();
+
+    _audioRecorder.closeRecorder();
+
+    super.dispose();
+  }
+}
+
+class AudioPlayerWidget extends StatefulWidget {
+  final String? audioPath;
+
+  AudioPlayerWidget({Key? key, this.audioPath}) : super(key: key);
+
+  @override
+  _AudioPlayerWidgetState createState() => _AudioPlayerWidgetState();
+}
+
+class _AudioPlayerWidgetState extends State<AudioPlayerWidget> {
+  late AudioPlayer _audioPlayer;
+  Duration _duration = Duration.zero;
+  Duration _position = Duration.zero;
+  bool _isPlaying = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _audioPlayer = AudioPlayer();
+    _setupAudioPlayer();
+  }
+
+  void _setupAudioPlayer() {
+    _audioPlayer.onDurationChanged.listen((newDuration) {
+      setState(() {
+        _duration = newDuration;
+      });
+    });
+
+    // Updated to use the correct API for position updates
+    _audioPlayer.onPositionChanged.listen((newPosition) {
+      setState(() {
+        _position = newPosition;
+      });
+    });
+
+    _audioPlayer.onPlayerComplete.listen((event) {
+      setState(() {
+        _position = Duration.zero;
+        _isPlaying = false;
+      });
+    });
+  }
+
+  void _togglePlay() async {
+    if (_isPlaying) {
+      await _audioPlayer.pause();
+    } else {
+      // Check if the audio file exists
+      final file = File(widget.audioPath!);
+      if (await file.exists()) {
+        // File exists, play the audio
+        await _audioPlayer.play(DeviceFileSource(file.path));
+      } else {
+        // File does not exist, handle the error
+        print('Audio file does not exist at path: ${widget.audioPath}');
+        // You might want to show an alert or a message to the user
+      }
+    }
+    setState(() {
+      _isPlaying = !_isPlaying;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // Set the height of the play button and slider thumb to fit within the 50 pixels.
+    final double buttonHeight = 30.0;
+    final double sliderHeight =
+        20.0; // Adjust as needed to fit within 50px height
+
+    return Container(
+      height: 50.0, // Container height fixed at 50 pixels
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Expanded(
+            child: SliderTheme(
+              data: SliderTheme.of(context).copyWith(
+                trackHeight: 2.0, // Adjust track height to be smaller
+                thumbShape:
+                    RoundSliderThumbShape(enabledThumbRadius: sliderHeight / 2),
+              ),
+              child: Slider(
+                value: _position.inSeconds.toDouble(),
+                min: 0.0,
+                max: _duration.inSeconds.toDouble(),
+                onChanged: (value) {
+                  final newPosition = Duration(seconds: value.toInt());
+                  _audioPlayer.seek(newPosition);
+                  setState(() {
+                    _position = newPosition;
+                  });
+                },
+              ),
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              IconButton(
+                icon: Icon(_isPlaying ? Icons.pause : Icons.play_arrow),
+                iconSize: buttonHeight,
+                onPressed: _togglePlay,
+              ),
+              SizedBox(
+                  width: 8.0), // Provides spacing between the button and text
+              Text(formatDuration(_position)),
+              Text(' / '),
+              Text(formatDuration(_duration)),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  String formatDuration(Duration duration) {
+    String twoDigits(int n) => n.toString().padLeft(2, "0");
+    String twoDigitMinutes = twoDigits(duration.inMinutes.remainder(60));
+    String twoDigitSeconds = twoDigits(duration.inSeconds.remainder(60));
+    return "${twoDigits(duration.inHours)}:$twoDigitMinutes:$twoDigitSeconds";
+  }
+
+  @override
+  void dispose() {
+    _audioPlayer.stop();
+    _audioPlayer.release();
+    _audioPlayer.dispose();
     super.dispose();
   }
 }
