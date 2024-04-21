c0b74f77cd30961aab45b15d9c12a6436a746d81
Oumaimatray <oumaymatray19@gmail.com>
2024-04-21 15:56:03 +0100

    filtrage tasks project

--- front_end/mobile_app/lib/project management/gestion de projet/listeProjet.dart
+++ front_end/mobile_app/lib/project management/gestion de projet/listeProjet.dart
@@ -4,7 +4,7 @@ class Project {
   final String title;
   final String startDate;
   final String endDate;
-  final int progress;
+  int _progress;
   final String createdBy;
   final List<Task> tasks;
 
@@ -12,22 +12,54 @@ class Project {
     required this.title,
     required this.startDate,
     required this.endDate,
-    required this.progress,
+    int progress = 0, // Default to 0 for initial progress
     required this.createdBy,
     required this.tasks,
-  });
+  }) : _progress = progress {
+    // When initializing the Project, calculate the initial progress
+    _calculateProgress();
+  }
+
+  // Getter for progress that calculates the percentage of completed tasks
+  int get progress {
+    return _progress;
+  }
+
+  // Method to calculate and update the progress
+  void _calculateProgress() {
+    if (tasks.isEmpty) {
+      _progress = 0;
+    } else {
+      int completedTasks = tasks.where((task) => task.isCompleted).length;
+      _progress = (completedTasks / tasks.length * 100).round();
+    }
+    // If you use a state management solution, you might want to notify listeners here as well
+  }
+
+  // Call this method when a task completion status changes
+  void updateTask(Task task) {
+    // Find the task and update it, if necessary
+    // As you already have the task reference you don't need to find it in the list
+
+    // Then recalculate the progress
+    _calculateProgress();
+
+    // If using a state management solution, notify listeners
+    // For example, if you're using ChangeNotifier, you would call notifyListeners() here
+  }
 }
 
 class Task with ChangeNotifier {
   String name;
   String dueDate;
   bool isCompleted;
+  String assignedTo;
 
-  Task({
-    required this.name,
-    required this.dueDate,
-    this.isCompleted = false,
-  });
+  Task(
+      {required this.name,
+      required this.dueDate,
+      this.isCompleted = false,
+      required this.assignedTo});
 
   void toggleCompleted() {
     isCompleted = !isCompleted;
@@ -41,18 +73,19 @@ class ListeProjet {
       title: 'Project 1',
       startDate: '15/10/2023',
       endDate: '20/10/2026',
-      progress: 54,
       createdBy: 'John Doe',
       tasks: [
         Task(
           name: 'Design Database Schema',
           dueDate: '30/10/2023',
           isCompleted: true,
+          assignedTo: 'oumayma',
         ),
         Task(
           name: 'Implement Authentication',
           dueDate: '15/11/2023',
           isCompleted: false,
+          assignedTo: 'haroun',
         ),
         // Add more tasks as needed
       ],
@@ -61,18 +94,19 @@ class ListeProjet {
       title: 'Project 2',
       startDate: '20/10/2026',
       endDate: '20/11/2026',
-      progress: 27,
       createdBy: 'Haroun',
       tasks: [
         Task(
           name: 'Initial Project Setup',
           dueDate: '25/10/2026',
           isCompleted: false,
+          assignedTo: 'oumayma',
         ),
         Task(
           name: 'Define Project Milestones',
           dueDate: '05/11/2026',
           isCompleted: false,
+          assignedTo: 'oumayma',
         ),
         // Add more tasks as needed
       ],
--- front_end/mobile_app/lib/project management/gestion de projet/project.dart
+++ front_end/mobile_app/lib/project management/gestion de projet/project.dart
@@ -3,96 +3,273 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:mobile_app/project%20management/gestion%20de%20projet/Project.dart';
 import 'package:mobile_app/project%20management/gestion%20de%20projet/listeProjet.dart'; // Ensure you have the correct import for Project
 
-class ProjectDetails extends StatelessWidget {
+class ProjectDetails extends StatefulWidget {
   final Project project;
 
   ProjectDetails({Key? key, required this.project}) : super(key: key);
 
   @override
+  _ProjectDetailsState createState() => _ProjectDetailsState();
+}
+
+class _ProjectDetailsState extends State<ProjectDetails> {
+  List<Task> _userTasks = [];
+  bool _showUserTasks = false;
+  @override
+  void initState() {
+    super.initState();
+    _filterUserTasks();
+  }
+
+  void _filterUserTasks() {
+    setState(() {
+      // Replace 'currentUser' with the actual user identifier of the logged-in user.
+      // For example, if you store the logged-in user details in a User object,
+      // you would compare task.assignedTo with User.id or a similar property.
+      String currentUser = "oumayma"; // Get the current user's id
+      _userTasks = widget.project.tasks.where((task) {
+        return task.assignedTo == currentUser;
+      }).toList();
+    });
+  }
+
+  void _onMenuSelected(String value) {
+    switch (value) {
+      case 'Edit':
+        // Handle edit action
+        break;
+      case 'Delete':
+        // Handle delete action
+        break;
+      case 'My Tasks':
+        setState(() {
+          _showUserTasks = true;
+          _filterUserTasks(); // Filter tasks when 'My Tasks' is selected
+        });
+        break;
+    }
+  }
+
+  void _updateTaskCompletion(Task task, bool isCompleted) {
+    setState(() {
+      // Find the task and update its completion status
+      final int taskIndex = widget.project.tasks.indexOf(task);
+      if (taskIndex != -1) {
+        widget.project.tasks[taskIndex].isCompleted = isCompleted;
+      }
+
+      // Recalculate the project progress
+      widget.project.updateTask(widget.project.tasks[taskIndex]);
+    });
+  }
+
+  int _calculateProgress(List<Task> tasks) {
+    int completedTasks = tasks.where((task) => task.isCompleted).length;
+    return (completedTasks / tasks.length * 100)
+        .round(); // Converts to percentage
+  }
+
+  @override
   Widget build(BuildContext context) {
+    List<Task> tasksToShow = _showUserTasks ? _userTasks : widget.project.tasks;
     return Scaffold(
-      appBar: AppBar(
-        title: Text(project.title,
-            style:
-                GoogleFonts.roboto(fontSize: 22, fontWeight: FontWeight.bold)),
-        backgroundColor:
-            Colors.indigo, // Prefer a color that matches your branding
-      ),
-      body: SingleChildScrollView(
-        padding: const EdgeInsets.all(16),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            DetailItem(
-              icon: Icons.date_range,
-              title: 'Start Date',
-              subtitle: project.startDate,
+        backgroundColor: Color(0xffC5A5FE),
+        appBar: AppBar(
+          title: Text(widget.project.title,
+              style: GoogleFonts.roboto(
+                  fontSize: 22, fontWeight: FontWeight.bold)),
+          backgroundColor: Color(0xff9155FD),
+          actions: <Widget>[
+            PopupMenuButton<String>(
+              onSelected: _onMenuSelected,
+              itemBuilder: (BuildContext context) {
+                return {'Edit', 'Delete', 'My Tasks'}.map((String choice) {
+                  return PopupMenuItem<String>(
+                    value: choice,
+                    child: Text(choice),
+                  );
+                }).toList();
+              },
             ),
-            DetailItem(
-              icon: Icons.date_range,
-              title: 'End Date',
-              subtitle: project.endDate,
-            ),
-            DetailItem(
-              icon: Icons.trending_up,
-              title: 'Progress',
-              subtitle: '${project.progress}%',
-            ),
-            SizedBox(height: 20),
-            Text(
-              'Project Tasks',
-              style:
-                  GoogleFonts.roboto(fontSize: 18, fontWeight: FontWeight.w500),
-            ),
-            ...project.tasks.map((task) => TaskItem(task: task)).toList(),
           ],
         ),
-      ),
-    );
+        body: RefreshIndicator(
+          onRefresh: () async {
+            // Call your method to fetch or refresh project details
+          },
+          child: SingleChildScrollView(
+            padding: const EdgeInsets.all(16),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                DetailItem(
+                  icon: Icons.date_range,
+                  title: 'Start Date',
+                  subtitle: widget.project.startDate,
+                ),
+                DetailItem(
+                  icon: Icons.date_range,
+                  title: 'End Date',
+                  subtitle: widget.project.endDate,
+                ),
+                DetailItem(
+                  icon: Icons.trending_up,
+                  title: 'Progress',
+                  subtitle: '${widget.project.progress}%',
+                  progress: widget.project.progress /
+                      100, // Pass the progress to the DetailItem widget
+                ),
+                SizedBox(height: 20),
+                Text(
+                  'Project Tasks',
+                  style: GoogleFonts.roboto(
+                      fontSize: 18, fontWeight: FontWeight.w500),
+                ),
+                ...widget.project.tasks
+                    .map((task) => TaskItem(
+                          task: task,
+                          onTaskCompletionChanged:
+                              (Task task, bool isCompleted) {
+                            _updateTaskCompletion(task, isCompleted);
+                          },
+                        ))
+                    .toList(),
+              ],
+            ),
+          ),
+        ));
   }
 }
 
-class DetailItem extends StatelessWidget {
+class DetailItem extends StatefulWidget {
   final IconData icon;
   final String title;
   final String subtitle;
+  final double? progress;
 
   const DetailItem({
     Key? key,
     required this.icon,
     required this.title,
     required this.subtitle,
+    this.progress,
   }) : super(key: key);
 
+  @override
+  _DetailItemState createState() => _DetailItemState();
+}
+
+class _DetailItemState extends State<DetailItem> {
   @override
   Widget build(BuildContext context) {
     return ListTile(
       contentPadding: EdgeInsets.symmetric(vertical: 8),
-      leading: Icon(icon, color: Colors.indigo),
-      title: Text(title,
+      leading: Icon(widget.icon, color: Colors.white),
+      title: Text(widget.title,
           style: GoogleFonts.roboto(fontSize: 16, fontWeight: FontWeight.w500)),
-      subtitle: Text(subtitle, style: GoogleFonts.roboto(fontSize: 14)),
+      subtitle: Text(widget.subtitle, style: GoogleFonts.roboto(fontSize: 14)),
+      trailing: widget.progress != null
+          ? SizedBox(
+              width: 100,
+              child: LinearProgressIndicator(
+                value: widget.progress,
+                backgroundColor: Colors.grey[300],
+                valueColor: AlwaysStoppedAnimation<Color>(Colors.indigo),
+              ),
+            )
+          : null,
     );
   }
 }
 
-class TaskItem extends StatelessWidget {
-  final Task task; // Your task model
+class TaskItem extends StatefulWidget {
+  final Task task;
+  final Function(Task task, bool isCompleted) onTaskCompletionChanged;
 
-  const TaskItem({Key? key, required this.task}) : super(key: key);
+  const TaskItem({
+    Key? key,
+    required this.task,
+    required this.onTaskCompletionChanged,
+  }) : super(key: key);
+
+  @override
+  _TaskItemState createState() => _TaskItemState();
+}
+
+class _TaskItemState extends State<TaskItem> {
+  late bool _isCompleted;
+
+  @override
+  void initState() {
+    super.initState();
+    _isCompleted = widget.task.isCompleted;
+  }
+
+// In your TaskItem stateful widget
+  void _toggleCompleted(bool newValue) {
+    // This line is no longer needed because we are using the newValue directly
+    // widget.task.toggleCompleted();
+
+    setState(() {
+      _isCompleted = newValue; // Update the local state with the new value
+    });
+
+    widget.onTaskCompletionChanged(
+        widget.task, _isCompleted); // Notify the parent widget of the change
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: EdgeInsets.symmetric(vertical: 8),
-      child: ListTile(
-        title: Text(task.name, style: GoogleFonts.roboto()),
-        subtitle: Text('Due by ${task.dueDate}', style: GoogleFonts.roboto()),
-        trailing: Checkbox(
-          value: task.isCompleted,
-          onChanged: (newValue) {
-            // Handle task completion toggle
-          },
+    return InkWell(
+      // Use InkWell for the ripple effect
+      onTap: () => _toggleCompleted(!_isCompleted),
+      child: Card(
+        margin: EdgeInsets.symmetric(vertical: 8),
+        child: Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: Row(
+            children: [
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      widget.task.name,
+                      style: GoogleFonts.roboto(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(height: 4),
+                    Text(
+                      'Due by ${widget.task.dueDate}',
+                      style: GoogleFonts.roboto(
+                        fontSize: 14,
+                        color: Colors.grey,
+                      ),
+                    ),
+                    if (widget.task.isCompleted) ...[
+                      SizedBox(height: 4),
+                      Chip(
+                        label: Text('Completed'),
+                        backgroundColor: Color(0xff9155FD),
+                      ),
+                    ],
+                  ],
+                ),
+              ),
+              Switch(
+                value: _isCompleted,
+                onChanged: _isCompleted
+                    ? _toggleCompleted
+                    : null, // Disable switch if task is not completed
+                activeColor: Colors.white,
+                activeTrackColor: Color(0xff9155FD),
+                inactiveThumbColor: Colors.white,
+                inactiveTrackColor: Colors.grey,
+              ),
+            ],
+          ),
         ),
       ),
     );
